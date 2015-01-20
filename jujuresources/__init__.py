c04037957ef6ed69d475cfc6befa966f87815530
Cory Johns <cory.johns@canonical.com>
2015-01-20 14:06:51 -0500

    Improved handling of resource destination

--- jujuresources/__init__.py
+++ jujuresources/__init__.py
@@ -6,24 +6,28 @@ from urllib import urlretrieve
 import yaml
 
 
+resources_cache = {}
+
+
 def _load_resources(resources_yaml):
     """
     Parse `resources.yaml` file and remap the values for easier use.
     """
-    with open(resources_yaml) as fp:
-        resources = yaml.load(fp).get('resources', {})
-    for name, resource in resources.iteritems():
-        filename = os.path.basename(urlparse(resource['url']).path)
-        resource.update({
-            'name': name,
-            'filename': filename,
-        })
-    return resources
+    if resources_yaml not in resources_cache:
+        with open(resources_yaml) as fp:
+            resources_cache[resources_yaml] = resdefs = yaml.load(fp)
+        output_dir = resdefs.get('options', {}).get('output_dir', 'resources')
+        for name, resource in resdefs['resources'].iteritems():
+            resource.setdefault(
+                'filename', os.path.basename(urlparse(resource['url']).path))
+            resource.setdefault(
+                'destination', os.path.join(output_dir, resource['filename']))
+    return resources_cache[resources_yaml]['resources']
 
 
-def verify_resources(resources_to_check=None, resources_yaml='resources.yaml', output_dir=None):
+def verify_resources(resources_to_check=None, resources_yaml='resources.yaml'):
     """
-    Verify if some or all of the resources were successfully fetched,
+    Verify if some or all resources previously fetched with :func:`fetch_resources`,
     including validating their cryptographic hash.
 
     :param list resources_to_check: A list of one or more resource names to
@@ -31,21 +35,16 @@ def verify_resources(resources_to_check=None, resources_yaml='resources.yaml', o
     :param str resources_yaml: Location of the yaml file containing the
         resource descriptions  Defaults to `resources.yaml` in the current
         directory.
-    :param str output_dir: Directory in which the fetched resources were placed.
-        Defaults to `resources/` in the current directory.
     :return: True if all of the resources are available and valid, otherwise False.
     """
     resources = _load_resources(resources_yaml)
-    if output_dir is None:
-        output_dir = 'resources'
     if resources_to_check is None:
         resources_to_check = resources.keys()
     for name in resources_to_check:
         resource = resources[name]
-        filename = os.path.join(output_dir, resource['filename'])
-        if not os.path.exists(filename):
+        if not os.path.exists(resource['destination']):
             return False
-        with open(filename) as fp:
+        with open(resource['destination']) as fp:
             hash = hashlib.new(resource['hash_type'])
             hash.update(fp.read())
             if resource['hash'] != hash.hexdigest():
@@ -53,7 +52,7 @@ def verify_resources(resources_to_check=None, resources_yaml='resources.yaml', o
     return True
 
 
-def fetch_resources(resources_yaml='resources.yaml', output_dir=None, base_url=None):
+def fetch_resources(resources_yaml='resources.yaml', base_url=None):
     """
     Attempt to fetch all resources for a charm.
 
@@ -65,6 +64,11 @@ def fetch_resources(resources_yaml='resources.yaml', output_dir=None, base_url=N
       * *hash* Cryptographic hash for the resource
       * *hash_type* Algorithm used to generate the hash; e.g., md5, sha512, etc.
 
+    The file may also contain an `options` section, which supports the
+    following options:
+
+      * *output_dir* Location for the fetched resources (default `./resources`)
+
     Note that errors fetching resources, incomplete or corrupted downloads,
     and other issues are silently ignored.  You should *always* call
     :func:`verify_resources` after this to confirm that everything was
@@ -73,25 +77,30 @@ def fetch_resources(resources_yaml='resources.yaml', output_dir=None, base_url=N
     :param str resources_yaml: Location of the yaml file containing the
         resource descriptions  Defaults to `resources.yaml` in the current
         directory.
-    :param str output_dir: Directory in which to place the fetched resources.
-        Defaults to `resources/` in the current directory.
-    :param str base_url: Base URL from which to fetch the resources.  If
-        given, only the filename from the resource descriptions are used,
-        with the rest of the URLs being ignored in favor of the given
+    :param str base_url: Override the location to fetch all resources from.
+        If given, only the filename from the resource definitions are used,
+        with the rest of the URL being ignored in favor of the given
         `base_url`.
     """
     resources = _load_resources(resources_yaml)
-    if output_dir is None:
-        output_dir = 'resources'
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
     for name, resource in resources.iteritems():
         if base_url:
             url = urljoin(base_url, resource['filename'])
         else:
             url = resource['url']
-        dest = os.path.join(output_dir, resource['filename'])
+        if url.startswith('./'):
+            url = url[2:]  # urlretrieve complains about this for some reason
+        if not os.path.exists(os.path.dirname(resource['dest'])):
+            os.makedirs(os.path.dirname(resource['destination']))
         try:
-            urlretrieve(url, dest)
+            urlretrieve(url, resource['destination'])
         except IOError:
             continue
+
+
+def resource_path(resource_name, resources_yaml='resources.yaml'):
+    """
+    Get the destination path for a named resource.
+    """
+    resources = _load_resources(resources_yaml)
+    return resources[resource_name]['destination']
